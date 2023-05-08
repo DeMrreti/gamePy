527258a154b651fda1891b94a113aea0cf3e7255
Diego Gualda De Marco <127225564+DeMrreti@users.noreply.github.com>
2023-05-08 14:57:50 +0200

    v1.1

--- main.py
+++ main.py
@@ -10,21 +10,31 @@ jugador = pyglet.graphics.Batch()
 
 x = 0
 y = 0
-pts = 0
+score = 0
 msg = 'SCORE:'
 fps_display = pyglet.window.FPSDisplay(main)
 
+tierra = shapes.Rectangle(0,0,400,400,(60,152,0), batch=gui)
+puntos = pyglet.text.Label(text=str(score),font_size=18,x=350,y=415,color=(255,255,255,255),align='right',batch=gui)
 linea = shapes.Line(0,401,400,401,1,(255,255,255),batch=gui)
 texto = pyglet.text.Label(text=msg,font_size=18,x=25,y=415,color=(255,255,255,255),batch=gui)
-puntos = pyglet.text.Label(text=str(pts),font_size=18,x=375,y=415,color=(255,255,255,255),align='right',batch=gui)
 circulo = shapes.Rectangle(x,y,50,50,(55,55,255),batch=jugador)
 star = generadorNivel.spawApples(manzanas)
 
-@main.event
-def colision() : 
-    if (x == (star.x-25) & y == (star.y-25)):
-        pts =+ 10
-        star.color = (0,0,0)
+def comprobarFronteras(x,y):
+    if x >= 400:
+        circulo.x = 0
+    elif x <= -50:
+        circulo.x = 350
+    if y >= 400:
+        circulo.y = 0
+    elif y <= -50:
+        circulo.y = 350
+
+def comprobarPuntos(x,y,x2,y2):
+    if x == x2-25 and y == y2-25:
+        star.color = (0,0,0,0)
+        score += 10
 
 @main.event
 def on_key_press(symbol, modifiers):
@@ -36,6 +46,8 @@ def on_key_press(symbol, modifiers):
         circulo.y += 50
     elif symbol == key.S:
         circulo.y -= 50
+    comprobarFronteras(circulo.x,circulo.y)
+    comprobarPuntos(circulo.x,circulo.y,star.x,star.y)
 
 @main.event
 def on_draw():
